9157eca0154eccb1e7cac5188c46b08c509ec3bf
Farhad <fbahrehmand82@gmail.com>
2022-04-05 18:20:14 -0700

    add

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ var projects = [
   {name: 'Pong', link: './projects/pong-ok', image: ''},
   {name: 'Expanding Pics', link: './projects/expanding-pic-ok', image: '/image/expand.png'},
   {name: 'Blurry Loading', link: './projects/blurry-ok', image: '/image/blurry.jpg'},
-  {name: 'Steps', link: './projects/steps-ok', image: '/image/steps.png'},
+  {name: 'Blurry Loading', link: './projects/blurry-ok', image: '/image/blurry.jpg'},
   {name: 'Steps', link: './projects/steps-ok', image: '/image/steps.png'},
   {name: 'Class Object Tutorial', link: './configs/class-ok', image: '/class.png'},
 ];
